cf149cb116c0483e74e5ec5e9451f98114855cad
yc791 <yc791@scarletmail.rutgers.edu>
2020-07-14 08:54:21 -0400

    Updates

---
+++ precache-manifest.81c325cbde218886956c956f036b3efa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9dc0d4d434dd9dfb21d7f0b96798a11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a590dd9f9c022454442",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "14405369070ae25641ce",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a590dd9f9c022454442",
+    "url": "/static/js/2.4c980e30.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4c980e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14405369070ae25641ce",
+    "url": "/static/js/main.c86b22a7.chunk.js"
+  },
+  {
+    "revision": "ab9a134594638b880197",
+    "url": "/static/js/runtime-main.cdbc005c.js"
+  }
+]);
